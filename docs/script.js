5e4762e8408d47e7a4e2d470b370ee34c75af7f4
Erica <77691605+erica63@users.noreply.github.com>
2022-02-23 17:04:57 +0000

    created pages and menu toggler

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,16 @@
-/* Loop to show multiple images to remove redundancy*/
+/* Creating a responsive menu */
+menuToggler.addEventListener('click', ev => {
+  menuToggler.classList.toggle('open');
+});
 
+for (const element of document.querySelectorAll('nav a')) {
+  element.addEventListener('click', ev => {
+    menuToggler.classList.remove('open');
+  });
+}
+
+
+/* Loop to show multiple images to remove redundancy*/
 var container = document.getElementById("graphics");
 
 for (var i =0, len = artists-images.length; i < len; ++i) {
